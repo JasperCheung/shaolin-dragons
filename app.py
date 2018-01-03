d7903c7bc9333d266697a20ceb56acdc4cce7bf5
Helen Ye <00yhelen@gmail.com>
2018-01-03 08:35:38 -0500

    Add test code for categories and game routes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 from flask import Flask, render_template, request, redirect, flash, Markup, url_for
 import requests, os
 from utils import database as db
+from utils import api
 
 app = Flask(__name__)
 app.secret_key = os.urandom(128)
@@ -14,7 +15,6 @@ app.secret_key = os.urandom(128)
 @app.route("/")
 def home():
     return render_template("home.html")
-
 # Displays the sign-up page
 # If credentials are valid, routes to game page
 @app.route("/sign_up")
@@ -26,6 +26,24 @@ def sign_up():
 @app.route("/log_in")
 def log_in():
     return render_template("login.html") # Page does not exist yet
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html", categories=api.CATEGORIES)
+
+@app.route("/game")
+def game():
+    args = request.args
+    if 'category' not in args:
+        flash("You must select a category to play the game", 'warning')
+        return redirect(url_for('categories'))
+    print "finding word"
+    category = args['category']
+    hyponyms = filter(api.valid_word, api.find_hyponyms(category))
+    word = api.random_word(hyponyms)
+    print word
+    gifs = api.find_gifs(word)
+    return render_template('game.html', gifs=gifs)
 
 if __name__ == "__main__":
     app.debug = True
