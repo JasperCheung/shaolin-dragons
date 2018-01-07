354a4f2218f4bfbe28bba02931265df4b73a7019
Valery Chen <cpan3@stuy.edu>
2018-01-07 15:31:04 -0500

    created stats methods

---
+++ utils/stats.py
@@ -0,0 +1,108 @@
+#Shaolin Dragons
+#Jasper Cheung, Shannon Lau, Carol Pan, Helen Ye
+#SoftDev pd7
+#P02 - 4Gifs1Word
+
+
+''' List of methods
+words_solved() - returns int
+num_users() - returns int
+num_words_flagged() - returns int
+num_gifs_flagged() - returns int
+next_high_score(user) - returns (String, int)
+                                  (other_user, next_higher_score)
+'''
+
+import sqlite3
+import database
+global db
+
+#WORDS SOLVED IN ALL OF HISTORY
+def words_solved():
+    global db
+    try:
+        c=database.open_db()
+        command = "SELECT * FROM history"
+        c.execute(command)
+        data = c.fetchall()
+        database.close_db()
+    except:
+        print "Error: could not get words solved"
+        return 0
+    count = 0
+    for entry in data:
+        count += 1
+    return count
+
+#NUMBER OF USERS IN ALL OF HISTORY
+def num_users():
+    global db
+    try:
+        c=database.open_db()
+        command = "SELECT * FROM accounts"
+        c.execute(command)
+        data = c.fetchall()
+        database.close_db()
+    except:
+        print "Error: could not get number of users"
+        return 0
+    count = 0
+    for entry in data:
+        count += 1
+    return count
+
+#NUMBER OF WORDS FLAGGED
+def num_words_flagged():
+    global db
+    try:
+        c=database.open_db()
+        command = "SELECT * FROM flaggedword"
+        c.execute(command)
+        data = c.fetchall()
+        database.close_db()
+    except:
+        print "Error: could not get number of words flagged"
+        return 0
+    count = 0
+    for entry in data:
+        count += 1
+    return count
+
+#NUMBER OF GIFS FLAGGED
+def num_gifs_flagged():
+    global db
+    try:
+        c=database.open_db()
+        command = "SELECT * FROM flaggedgif"
+        c.execute(command)
+        data = c.fetchall()
+        database.close_db()
+    except:
+        print "Error: could not get number of gifs flagged"
+        return 0
+    count = 0
+    for entry in data:
+        count += 1
+    return count
+
+#GET USER WITH NEXT HIGHER SCORE
+def next_high_score(user):
+    global db
+    try:
+        score = database.get_score(user)
+        c=database.open_db()
+        command = "SELECT user,pts FROM accounts WHERE pts>? ORDER BY pts ASC"
+        c.execute(command, (score,))
+        data = c.fetchone()
+        database.close_db()
+    except:
+        print "Error: could the next higher score"
+        return None
+    return data
+
+
+#print words_solved()
+#print num_users()
+#print num_words_flagged()
+#print num_gifs_flagged()
+#print next_high_score("jack")
