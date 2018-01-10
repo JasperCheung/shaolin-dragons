d230bd7e9a5736f4b877397500520dc099b9bf6a
slau8 <slau8@stuy.edu>
2018-01-10 15:07:53 -0500

    adding static files

---
+++ static/js/main.js
@@ -0,0 +1,172 @@
+var key = ""; // Word to achieve; provided by python
+var bankSize = 12;
+var bankLetters = []; // Letters in the word bank; provided by python
+var wordIndex = []; // Indexes of the letter guessed
+var category = "";
+
+var retrieveKey = function(e){
+  $.ajax({
+    url: "/play",
+    type: "GET",
+    data: {},
+    success: function(d) {
+      console.log(d);
+      d = JSON.parse(d);
+      key = d["word"];
+    }
+  })
+};
+
+var setCategory = function(){
+  category = location.search.substring(1).split("=")[1]
+};
+
+// Fill wordIndex with index values
+// -1 denotes an empty slot
+var fillWordIndex = function(){
+  for (var i in key){
+    wordIndex.push(-1);
+  }
+};
+
+// Retrieves bank letters and fills bankLetters array
+var fillBankLetters = function(){
+  for (var i = 0; i < bankSize; i++){
+    var id = "bank" + i;
+    letter = document.getElementById(id);
+    bankLetters[i] = letter.innerHTML;
+  }
+};
+
+// Check if word is complete
+var isFull = function(){
+  for (var i in wordIndex){
+    if (wordIndex[i] == -1)
+      return false;
+  }
+  return true;
+};
+
+// Check for word match when word is complete
+var checkWord = function(){
+  guess = "";
+  for (var i in wordIndex){
+    guess += bankLetters[wordIndex[i]];
+  }
+  if (key === guess)
+    correctWord();
+  else
+    returnLetters();
+};
+
+// Send all letters to "bank"
+var returnLetters = function(){
+  for (var i in bankLetters){
+    var id = "bank" + i;
+    letter = document.getElementById(id);
+    letter.innerHTML = bankLetters[i];
+    letter.setAttribute("class","card card-letter card-animate");
+  }
+  for (var i in wordIndex){
+    wordIndex[i] = -1;
+    var id = "word" + i;
+    letter = document.getElementById(id);
+    letter.innerHTML = "";
+    letter.setAttribute("class","card-pressed card-letter");
+  }
+};
+
+// Add 100 points to database via python
+// Load new word in the same category
+var correctWord = function(){
+  $.ajax({
+    url: "/win",
+    type: "GET",
+    data: {"word" : key, "category" : category},
+    success: function(d){
+      return;
+    }
+  })
+};
+
+// Return leftmost index of wordIndex whose value is -1
+var leftIndex = function(){
+  for (var i in wordIndex){
+    if (wordIndex[i] == -1)
+    return i;
+  }
+  return -1;
+};
+
+// Run when "bank letters" are clicked
+// Send letter to guessed section
+var bankCallback = function(e){
+  if (this.getAttribute("class").equals("card card-letter card-animate")){
+    this.innerHTML = "";
+    this.setAttribute("class","card-pressed card-letter");
+    // extracting # from id = "bank#" and inserting into wordIndex
+    var i = this.getAttribute("id")[4];
+    var j = leftIndex();
+    wordIndex[j] = i;
+    letter = document.getElementById("word" + j);
+    letter.innerHTML = bankLetters[i];
+    letter.setAttribute("class","card card-letter");
+  }
+};
+
+// Run when "guessed letters" are clicked
+// Send letter back to bank
+var wordCallback = function(e){
+  if (this.getAttribute("class").equals("card card-letter")){
+    this.innerHTML = "";
+    this.setAttribute("class","card-pressed card-letter");
+    var i = this.getAttribute("id")[4];
+    var j = wordIndex[i]; // j is index of letter in bankLetters
+    wordIndex[i] = -1;
+    letter = document.getElementById("bank" + j);
+    letter.innerHTML = bankLetters[j];
+    letter.setAttribute("class","card card-letter card-animate");
+  }
+};
+
+// Add listeners to bank letters
+var addBankListeners = function(){
+  for (var i in bankLetters){
+    var id = "bank" + i;
+    var letter = document.getElementById(id);
+    letter.addEventListener("click", bankCallback);
+  }
+};
+
+// Add listeners to guessed letters
+var addWordListeners = function(){
+  for (var i in wordIndex){
+    var id = "word" + i;
+    var letter = document.getElementById(id);
+    letter.addEventListener("click", wordCallback);
+  }
+};
+
+// Add listener to return button
+var addReturnListeners = function(){
+  var button = document.getElementById("return");
+  button.addEventListener("click", returnLetters);
+};
+
+// Add all event listeners
+var addEventListeners = function(){
+  addBankListeners();
+  addWordListeners();
+  addReturnListeners();
+};
+
+// Word play setup
+var setUp = function(){
+  retrieveKey();
+  setCategory();
+  fillWordIndex();
+  fillBankLetters();
+  addEventListeners();
+};
+
+setUp();
